e5e3c541e66d29cbdd56ffa857e29c464ff3e8a9
13ubbleBoy <imyourproblem256@gmail.com>
2026-08-03 19:57:59 +0530

    Added 08_abstraction

---
+++ 08_abstraction/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+
+<*> Abstraction
+--> Show only the necessary behavior and hide the implementation details.
+
+01. Abstract Class
+--> An abstract class cannot have objects.
+    It can have variables.
+
+02. Abstract Methods
+--> An abstract have no implementation.
+    The child class must implement all abstract methods.
+
+*/
+
+Player p = new Player();
+Console.WriteLine(p.health);   // 100
+
+abstract class Character
+{
+    public int health = 100;   // Variable
+
+    public abstract void Attack();
+}
+
+class Player : Character
+{
+    public override void Attack()
+    {
+        Console.WriteLine("Player Attacks");
+    }
+}
