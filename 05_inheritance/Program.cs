233458dab1e7ba5e25dbd50b5723a4de5dc72fd9
13ubbleBoy <imyourproblem256@gmail.com>
2026-08-03 20:12:55 +0530

    Added 09_interface

--- 05_inheritance/Program.cs
+++ 05_inheritance/Program.cs
@@ -1,4 +1,25 @@
-﻿Player player = new Player();
+﻿/*
+
+<*> Types of inheritance:
+
+01. Single Inheritance
+    Ex: class Character { }
+        class Player : Character { }
+
+02. Multilevel Inheritance
+    Ex: class Character { }
+        class Enemy : Character { }
+        class Boss : Enemy { }
+
+03. Hierarchical Inheritance
+    Ex: class Character { }
+        class Player : Character { }
+        class Enemy : Character { }
+        class NPC : Character { }
+
+*/
+
+Player player = new Player();
 
 player.Move();
 player.Attack();
---
+++ 09_interface/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+
+<*> Interface
+--> An interface is a contract (or blueprint) that tells a class what
+    methods or properties it must have, but not how they work.
+    The class provides the implementation.
+
+01. Cannot have variables.
+02. A class implements an interface or multiple.
+
+------------------------------------------------------------------------------
+
+Abstract Class	                         Interface
+
+Used when classes share common code      Used when classes share only a 
+and data.                                capability/behavior.  
+
+
+Can have implemented methods, fields,    Cannot have instance fields or
+and constructors.	                     constructors; mainly defines a contract.
+
+A class can inherit only one abstract    A class can implement multiple
+class.	                                 interfaces.
+
+*/
+interface IAnimal
+{
+    void Sound();   // Method declaration (no body)
+}
+
+interface IHeal
+{
+    void Heal();
+}
+
+class Dog : IAnimal, IHeal
+{
+    public void Sound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IAnimal animal = new Dog();
+        animal.Sound();
+    }
+}
