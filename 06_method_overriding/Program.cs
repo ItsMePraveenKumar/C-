761aeade46f393409bec52223ca30b1b9ffffdab
13ubbleBoy <imyourproblem256@gmail.com>
2026-08-03 19:32:17 +0530

    Added 06_method_overriding

---
+++ 06_method_overriding/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+
+01. virtual
+--> The parent class marks a method as virtual means a child class is 
+    allowed to replace this method.
+
+02. override
+--> The child class replaces the parent's method.
+
+*/
+
+Player player = new Player();
+player.Attack();
+
+class Character
+{
+    public virtual void Attack()
+    {
+        Console.WriteLine("Character Attacks");
+    }
+}
+
+class Player : Character
+{
+    public override void Attack()
+    {
+        base.Attack(); // call parent class's method.
+        
+        Console.WriteLine("Player Attacks");
+    }
+}
