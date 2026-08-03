08c8de6a21e39b6f9d147e515db0baa57f1c91d1
13ubbleBoy <imyourproblem256@gmail.com>
2026-08-03 21:06:54 +0530

    Added 10_encapsulation

---
+++ 10_encapsulation/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+
+<*> Encapsulation
+--> Encapsulatie similar functionalities together.
+    Encapsulation means hiding data and controlling how it is accessed.
+
+*/
+
+Player player = new Player();
+player.Health = 80;
+Console.WriteLine(player.Health);
+
+class Player
+{
+    public int Health { get; set; } = 100;
+}
+
+/*
+
+public int Health { get; set; } = 100;
+
+----------------------------------------
+
+private int _health = 100;
+public int Health
+{
+    get
+    {
+        return _health;
+    }
+
+    set
+    {
+        _health = value;
+    }
+}
+
+*/
