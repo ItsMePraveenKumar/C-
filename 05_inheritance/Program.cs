b67e5f2d756acd88bcb6bcbab5bde009ed284948
13ubbleBoy <imyourproblem256@gmail.com>
2026-08-03 17:36:05 +0530

    Added 05_inheritance

---
+++ 05_inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿Player player = new Player();
+
+player.Move();
+player.Attack();
+
+class Character
+{
+    public void Move()
+    {
+        Console.WriteLine("Moving");
+    }
+}
+
+class Player : Character // Player class inherited Character class.
+{
+    public void Attack()
+    {
+        Console.WriteLine("Attacking");
+    }
+}
