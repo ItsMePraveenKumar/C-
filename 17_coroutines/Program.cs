3ee715e846a621bdd3ec6bdbf366ed0bc69f441b
13ubbleBoy <imyourproblem256@gmail.com>
2026-08-04 13:07:09 +0530

    Added 17_coroutines

---
+++ 17_coroutines/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+
+<*> Coroutines
+--> A Coroutine lets you pause a method and continue it later without 
+    blocking the further code/game.
+
+*/
+
+using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        IEnumerator coroutine = MyCoroutine();
+
+        while (coroutine.MoveNext())
+        {
+            Console.WriteLine(coroutine.Current);
+        }
+    }
+
+    static IEnumerator MyCoroutine()
+    {
+        Console.WriteLine("Start");
+
+        yield return "Waiting...";
+
+        Console.WriteLine("End");
+    }
+}
