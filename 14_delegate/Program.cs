def561b103e92ace234551fcb62498e0018053a2
13ubbleBoy <imyourproblem256@gmail.com>
2026-08-04 11:14:20 +0530

    Added 14_delegate

---
+++ 14_delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+
+<*> Delegate
+--> A delegate is a variable that can store a reference to a method.
+
+*/
+MyDelegate action = Jump;
+action += Attack;
+
+action(); // Calls both function 1 after another
+
+void Jump()
+{
+    Console.WriteLine("Jump");
+}
+
+void Attack()
+{
+    Console.WriteLine("Attack");
+}
+
+delegate void MyDelegate();
